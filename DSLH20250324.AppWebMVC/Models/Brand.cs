113dd9f11af833f2ae96e0cc357fc960f0bf2d67
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-03-24 11:52:48 -0300

    Se implemetaron los DataAnnotations

--- DSLH20250324.AppWebMVC/Models/Brand.cs
+++ DSLH20250324.AppWebMVC/Models/Brand.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DSLH20250324.AppWebMVC.Models;
 
 public partial class Brand
 {
+    [Display(Name = "IdMarca")]
     public int Id { get; set; }
-
+    [Display(Name = "Nombre")]
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
     public string BrandName { get; set; } = null!;
+    [Display(Name = "País")]
 
     public string? Country { get; set; }
 
--- DSLH20250324.AppWebMVC/Models/Product.cs
+++ DSLH20250324.AppWebMVC/Models/Product.cs
@@ -1,27 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DSLH20250324.AppWebMVC.Models;
 
 public partial class Product
 {
+    [Display(Name = "IdProducto")]
     public int Id { get; set; }
-
+    [Display(Name = "Nombre")]
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
     public string ProductName { get; set; } = null!;
-
+    [Display(Name = "Descripcion")]
     public string? Description { get; set; }
-
+    [Display(Name = "Precio")]
+    [Required(ErrorMessage = "El precio es obligatorio.")]
     public decimal Price { get; set; }
 
     public decimal PurchasePrice { get; set; }
 
+    [Display(Name = "Bodega")]
     public int? WarehouseId { get; set; }
 
+    [Display(Name = "Marca")]
     public int? BrandId { get; set; }
 
+    [Display(Name = "Notas")]
     public string? Notes { get; set; }
-
+    [Display(Name = "Marca")]
     public virtual Brand? Brand { get; set; }
-
+    [Display(Name = "Bodega")]
     public virtual Warehouse? Warehouse { get; set; }
 }
